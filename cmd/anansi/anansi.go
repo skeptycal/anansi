8f1782daf0c620a010f1769082e4dc2f8064af3e
skeptycal <skeptycal@gmail.com>
2020-12-04 14:21:07 -0600

    add logrus

--- cmd/anansi/anansi.go
+++ cmd/anansi/anansi.go
@@ -4,9 +4,22 @@ import (
 	"io"
 	"os"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/skeptycal/anansi"
 )
 
 func main() {
+	Formatter := new(log.TextFormatter)
+	Formatter.TimestampFormat = "02-01-2006 15:04:05"
+	Formatter.FullTimestamp = true
+	log.SetFormatter(Formatter)
+
+	log.Info("Some info. Earth is not flat.")
+	log.Warning("This is a warning")
+	log.Error("Not fatal. An error. Won't stop execution")
+	log.Fatal("MAYDAY MAYDAY MAYDAY. Execution will be stopped here")
+	log.Panic("Do not panic")
 	io.Copy(anansi.Output, os.Stdin)
+
 }
