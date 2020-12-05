8886e900e2350d13ca26848cf76801752f45dd9a
skeptycal <skeptycal@gmail.com>
2020-12-04 23:45:59 -0600

    add logrus

--- util.go
+++ util.go
@@ -8,44 +8,77 @@ import (
 	db "github.com/skeptycal/godebug"
 )
 
-func init() {
-	log.Println("anansi logging started")
-}
-
-// tableFormat defines cli table formatting options
-type tableFormat struct {
-	TopHeader       string
-	BottomHeader    string
-	MiddleHeader    string
-	SideBorder      string
-	DebugHeader     string
-	AlternateBorder bool
-	AlternateColor  bool
-	HeaderColor     ansiCodes
-	RowColors       []ansiCodes
-	BorderColor     ansiCodes
-}
-
-// codeSymbolColors defines the ANSI colors used for 'pretty' code output
-type codeSymbolColors struct {
-	Keywords    Attribute
-	Operator    Attribute
-	Integers    Attribute
-	Floats      Attribute
-	Strings     Attribute
-	Comments    Attribute
-	Punctuation Attribute
-	Types       Attribute
-}
-
-type defaultValues struct {
-	ScreenWidth      int
-	UseColor         bool
-	TableFormat      tableFormat
-	CodeSymbolColors codeSymbolColors
-}
-
 var config db.Session
+
+func init() {
+	log.Debug("anansi logging started")
+
+}
+
+func br() (n int, err error) {
+	return fmt.Println("")
+}
+
+func hr() (n int, err error) {
+	// todo fix this
+	return fmt.Println("")
+}
+
+// TableFormat defines cli table formatting options
+type TableFormat struct {
+	TopHeader    string
+	BottomHeader string
+	MiddleHeader string
+	// SideBorder      string
+	// DebugHeader     string
+	// AlternateBorder bool
+	// AlternateColor  bool
+	// HeaderColor     ansiCodes
+	// RowColors       []ansiCodes
+	// BorderColor     ansiCodes
+}
+
+// Tokens defines the ANSI colors used for 'pretty' code output
+type Tokens map[string]Attribute
+
+// New returns a new Tokens object
+func (t *Tokens) New(m map[string]Attribute) Tokens {
+    var defaultTokens = make(Tokens)
+
+
+	defaultTokens = map[string]Attribute
+		"colorKeywords":    FgCyan,
+		"colorOperator":    FgRed,
+		"colorIntegers":    FgMagenta,
+		"colorFloats":      FgMagenta,
+		"colorStrings":     FgHiGreen,
+		"colorComments":    FgGreen,
+		"colorPunctuation": FgWhite,
+		"colorTypes":       FgYellow,
+	}
+	if m == nil {
+		return defaultTokens
+	}
+	return m
+}
+
+// CliConfig defines the configuration for CLI output
+type CliConfig struct {
+	ScreenWidth int
+	UseColor    bool
+	TableFormat
+	Tokens
+}
+
+// New returns a new CliConfig object
+func (c *CliConfig) New() *CliConfig {
+	return &CliConfig{
+		ScreenWidth: 79,
+		UseColor:    true,
+		TableFormat: TableFormat.New(),
+		Tokens:      Tokens.New(),
+	}
+}
 
 // VerboseLevel defines the level of visual feedback in the cli terminal
 type VerboseLevel uint
@@ -75,12 +108,4 @@ func StartTimer(name string) func() {
 		d := time.Now().Sub(t)
 		log.Info(name, "took", d)
 	}
-}
-
-func br() (n int, err error) {
-	return fmt.Println("")
-}
-
-func hr() (n int, err error) {
-	return fmt.Println("")
 }
